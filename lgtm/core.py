4cb5abca8cd24e07e3c095b64ca590323fc5dfff
MacTamaoki <40863457+MacTamaoki@users.noreply.github.com>
2021-02-04 14:13:38 +0900

    coreモジュールのアップデート

--- lgtm/core.py
+++ lgtm/core.py
@@ -1,11 +1,13 @@
 import click
 
 @click.command()
-def cli():
+@click.option('--message', '-m', default='LGTM', show_default=True, help='画像にのせる文字列')
+@click.argument('keyword')
+def cli(keyword, message):
     """LGBT画像生成ツール"""
-    lgtm()
-    click.echo('lgtm')
+    lgtm(keyword, message)
+    click.echo('lgtm') #動作確認用
 
-def lgtm():
+def lgtm(keyword, message):
     #ここにロジックを書く
     pass
