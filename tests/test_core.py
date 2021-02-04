c80bf0761e74210159127e7d86559afacb142750
MacTamaoki <40863457+MacTamaoki@users.noreply.github.com>
2021-02-04 14:15:37 +0900

    テストコードの修正

--- tests/test_core.py
+++ tests/test_core.py
@@ -3,4 +3,4 @@ import unittest
 class LgmtTest(unittest.TestCase):
     def test_lgtm(self):
         from lgtm.core import lgtm
-        self.assertIsNone(lgtm())
+        self.assertIsNone(lgtm('./python.jpeg', 'LGTM'))
